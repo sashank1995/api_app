b02fbb816ecb8c4a6ea3ab79aacf463e32dc2761
sashank1995 <challasashank@gmail.com>
2016-07-04 03:55:25 +0530

    Adds public product endpoints(show, index)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   before_create :generate_authentication_token!
-  has_many :products, dependant: :destroy
+  has_many :products, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
-  root 'home#index'
   devise_for :users
   namespace :api, defaults: {format: :json}, constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
       resources :sessions, :only => [:create, :destroy]
+      resources :products, :only => [:show, :index, :create, :update, :destroy]
     end
   end
 end
